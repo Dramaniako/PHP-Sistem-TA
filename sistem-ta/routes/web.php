59315d81ad40e8edd081630514c8b15853fef3a9
AsepNwby <mardjokoasep@gmail.com>
2025-12-29 15:06:22 +0800

    Manajemen Profile

--- sistem-ta/routes/web.php
+++ sistem-ta/routes/web.php
@@ -4,6 +4,31 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PenjadwalanController;
 use Illuminate\Support\Facades\Route;
 
+//Manajemen Profile 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
+
+Route::get('/', [PageController::class, 'welcome']);
+Route::get('/about', [PageController::class, 'about']);
+
+Route::get('/login', [PageController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
+//
+
 Route::get('/', function () {
     return view('welcome');
 });
